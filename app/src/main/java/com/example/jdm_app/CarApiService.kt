7771b6b64fa288aae7bb6da0a37484d4ccdcea3f
0pilatos0 <Paulvanderlei@hotmail.nl>
2023-01-03 23:32:20 +0100

    preparation for other api calls

--- app/src/main/java/com/example/jdm_app/CarApiService.kt
+++ app/src/main/java/com/example/jdm_app/CarApiService.kt
@@ -10,6 +10,24 @@ import retrofit2.http.*
 interface CarApiService {
     @GET("car")
     suspend fun getCars(): Response<List<Car>>
+
+    @POST("car")
+    suspend fun createCar(@Body car: Car): Response<Car>
+
+    @PUT("car/{id}")
+    suspend fun updateCar(@Path("id") id: Int, @Body car: Car): Response<Car>
+
+    @DELETE("car/{id}")
+    suspend fun deleteCar(@Path("id") id: Int) : Response<Void>
+
+    @GET("car/{id}")
+    suspend fun getCar(@Path("id") id: Int): Response<Car>
+
+    @GET("car/tco/{id}")
+    suspend fun getCarTCO(@Path("id") id: Int): Response<Double>
+
+    @GET("car/cpk/{id}")
+    suspend fun getCarCPK(@Path("id") id: Int): Response<Double>
 }
 
 private val BASE_URL = "https://c695-2a02-a450-954b-1-3ccc-e0a-a40f-c513.eu.ngrok.io/"
