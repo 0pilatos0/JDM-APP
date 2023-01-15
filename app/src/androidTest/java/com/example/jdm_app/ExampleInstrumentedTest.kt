4d5b91fb9a67931f72ae177fe5b34c45a8cd15c4
Xeno <xenocidechan@gmail.com>
2023-01-15 22:46:07 +0100

    Make UI tests and fixed some issues

--- app/src/androidTest/java/com/example/jdm_app/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/jdm_app/ExampleInstrumentedTest.kt
@@ -1,10 +1,11 @@
 package com.example.jdm_app
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.PositionAssertions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.example.jdm_app.activity.MainActivity
@@ -18,18 +19,41 @@ import org.junit.Rule
 
 
 
-@RunWith(AndroidJUnit4)
+@RunWith(AndroidJUnit4::class)
 @LargeTest
-class HelloWorldEspressoTest {
+class EspressoTest {
 
     @get:Rule
-    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+    val activityRule = ActivityScenarioRule(MainActivity::class.java) //, ReservationActivity::class.java
 
-    class ActivityScenarioRule(java: Class<MainActivity>) {
-
+    @Test
+    fun navigationBarIsDisplayedAndWorking() {
+        onView(withId(R.id.bottom_navigation_view)).check(matches(isDisplayed()))
+        onView(withId(R.id.action_reservation)).perform(ViewActions.click())
+        onView(withId(R.id.bottom_navigation_view)).check(matches(isDisplayed()))
+        onView(withId(R.id.action_profile)).perform(ViewActions.click())
+        onView(withId(R.id.bottom_navigation_view)).check(matches(isDisplayed()))
+        onView(withId(R.id.action_cars)).perform(ViewActions.click())
     }
 
-    @Test fun listGoesOverTheFold() {
-        onView(withText("Delete")).check(matches(isDisplayed()))
+    @Test
+    fun carCreateIsWorking() {
+        onView(withId(R.id.my_cars_button)).perform(ViewActions.click())
+        onView(withText("New Car")).check(matches(isDisplayed()))
+        onView(withText("New Car")).perform(ViewActions.click())
+        onView(withId(R.id.action_save)).perform(ViewActions.click())
+    }
+
+    @Test
+    fun rentCarIsWorking() {
+        onView(withText("Chevrolet")).perform(ViewActions.click())
+        onView(withText("Rent")).perform(ViewActions.click())
+        onView(withId(R.id.edit_date_rent_date)).perform(ViewActions.typeText("2023-02-03"))
+        onView(withId(R.id.edit_date_return_date)).perform(ViewActions.typeText("2023-02-10"))
+        onView(withId(R.id.edit_text_terms_conditions)).perform(ViewActions.typeText("These are the terms and conditions!"))
+        onView(withId(R.id.edit_text_postal_code)).perform(ViewActions.typeText("1234AB"))
+        onView(withId(R.id.edit_text_house_number)).perform(ViewActions.typeText("1225"))
+        onView(withId(R.id.action_save)).perform(ViewActions.click())
     }
 }
+// onView(withText("My Cars")).check(matches(isDisplayed()))
